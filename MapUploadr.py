5115a517b4a1941998f648e0e2564fbff60cbecc
zhiqu <zhiqu@yahoo-inc.com>
2018-08-23 13:02:36 -0700

    Add MapUploadr & output

--- MapUploadr.py
+++ MapUploadr.py
@@ -14,12 +14,23 @@ with open('api_key.txt', 'r') as apiKeyFile:
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
 # Open csv file for output
+target_file = 'target/TitleLookupTableFile.csv'
 if not os.path.exists('target'):
     os.makedirs('target')
-TitleLookupTableFile = csv.writer(open("target/TitleLookupTableFile.csv", "w"))
+
+# Start from last checkpoint
+uploaded_images = []
+with open(target_file, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        uploaded_images.append(row['file_name'])
+TitleLookupTableFile = csv.writer(open(target_file, 'a'))
+if not uploaded_images:
+    TitleLookupTableFile.writerow(['file_name', 'image_id', 'url'])
+else:
+    print('uploaded_images', uploaded_images)
 
 print('Step 1: authenticate')
-
 # Only do this if we don't have a valid token already
 if not flickr.token_valid(perms='write'):
     # Get a request token
@@ -39,21 +50,27 @@ if not flickr.token_valid(perms='write'):
 
 print('Step 2: Upload and write csv')
 map_dir = '/Users/zhiqu/PersonalToolNodeJs/public/resources/map1/'
-counter = 0
+counter = len(uploaded_images)
 for file_name in os.listdir(map_dir):
-    if file_name.endswith(".jpg"):
-        resp = flickr.upload(filename=map_dir + file_name,
-                             title=file_name,
-                             description='East Asia Map of year ' + file_name[:-4])
-        image_id = resp.findtext('photoid')
-        json_res = flickr.photos.getSizes(photo_id=image_id, format='json')
-        parsed_json = json.loads(json_res)
-        size_list = parsed_json['sizes']['size']
-        url = ''
-        for size_item in size_list:
-            if size_item['label'] == 'Original':
-                url = size_item['source']
-        TitleLookupTableFile.writerow([basename(file_name), image_id, url])
+    if file_name.endswith('.jpg') and file_name not in uploaded_images:
+        for i in range(0, 3):
+            try:
+                resp = flickr.upload(filename=map_dir + file_name,
+                                     title=file_name,
+                                     description='East Asia Map of year ' + file_name[:-4],
+                                     tags='Asia Map')
+                image_id = resp.findtext('photoid')
+                json_res = flickr.photos.getSizes(photo_id=image_id, format='json')
+                parsed_json = json.loads(json_res)
+                size_list = parsed_json['sizes']['size']
+                url = ''
+                for size_item in size_list:
+                    if size_item['label'] == 'Original':
+                        url = size_item['source']
+                TitleLookupTableFile.writerow([basename(file_name), image_id, url])
+                break
+            except Exception:
+                print('Exception happened')
         counter += 1
         if counter % 10 == 0:
-            print('Done uploading image (', counter, 'out of 2180)')
+            print('Done uploading image', counter, 'out of 2180')
